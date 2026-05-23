6cf13c619aca5fe85282d9853ec6f02bf5e797ab
Maquito365 <macsoloj@gmail.com>
2026-05-23 12:34:53 -0600

    Creación de clases para la "app" y 4 implementaciones hecha login, home, search e index

---
+++ js/index.js
@@ -0,0 +1,30 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80
+    },
+    color: {
+      value: "#39FF14"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#39FF14",
+      opacity: 0.3,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  }
+});
---
+++ js/search.js
@@ -0,0 +1,35 @@
+/* ========================================= */
+/* SEARCH.JS */
+/* ========================================= */
+
+particlesJS("particles-js", {
+
+    particles: {
+        number: {
+            value: 70
+        },
+        color: {
+            value: "#39FF14"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.3
+        },
+        size: {
+            value: 2
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#39FF14",
+            opacity: 0.2,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 1
+        }
+    }
+});
